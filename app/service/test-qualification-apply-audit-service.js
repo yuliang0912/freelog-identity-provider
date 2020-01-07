ea5a4a72f2217d332dd124128afac61898a85f7f
yuliang <yu.liang@freelog.com>
2020-01-07 15:21:24 +0800

    1.修改邮件通知模板

--- app/service/test-qualification-apply-audit-service.js
+++ app/service/test-qualification-apply-audit-service.js
@@ -68,16 +68,34 @@ module.exports = class TestQualificationApplyAuditService extends Service {
     getEmailTemplateContent(templateCode, templateParam) {
         switch (templateCode) {
             case 'SMS_181430088':
-                return `Hi [${templateParam.username}]，
-                感谢您的支持！您的内测申请已通过。立即体验内测版本，请点击https://console.freelog.com/。
-                使用中有任何问题或建议，欢迎您到我们的官方论坛https://forum.freelog.com留言！
-                您真诚的，
-                Freelog团队`
+                return `<!DOCTYPE html>
+                         <html lang="en">
+                          <head>
+                              <meta charset="UTF-8">
+                          </head>
+                          <body>
+                              <div>
+                                  <p>Hi ${templateParam.username}，</p>
+                                  <p>感谢您的支持！您的内测申请已通过。立即体验内测版本，请点击<a href="https://console.freelog.com/" target="_blank">https://console.freelog.com/</a>。</p>
+                                  <p>使用中有任何问题或建议，欢迎您到我们的官方论坛<a href="https://forum.freelog.com/" target="_blank">https://forum.freelog.com/</a>留言！</p>
+                                  <p>您真诚的，<br/>Freelog团队 </p>
+                              </div>
+                          </body>
+                        </html>`
             case 'SMS_181192172':
-                return `Hi [${templateParam.username}]，
-                感谢您的支持！很遗憾，您的内测申请未通过。重新提交申请，请点击[内测申请提交页面网址]。
-                您真诚的，
-                Freelog团队`
+                return `<!DOCTYPE html>
+                         <html lang="en">
+                          <head>
+                              <meta charset="UTF-8">
+                          </head>
+                          <body>
+                             <div>
+                                <p>Hi ${templateParam.username}，</p>
+                                <p>感谢您的支持！很遗憾，您的内测申请未通过。重新提交申请，请点击<a target="_blank" href="https://console.freelog.com/alpha-test/apply">https://console.freelog.com/alpha-test/apply</a>。</p>
+                                <p>您真诚的，<br/>Freelog团队 </p>
+                            </div>
+                          </body>
+                        </html>`
         }
     }
 }
