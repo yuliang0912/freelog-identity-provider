2e80cd5e6a64541e64cdb9e00a20b2c92b5508d7
yuliang <4896819@qq.com>
2021-09-27 15:48:51 +0800

    1.提交代码

--- src/app/controller/user-info.ts
+++ src/app/controller/user-info.ts
@@ -238,7 +238,7 @@ export class UserInfoController {
     async updateUserInfo() {
         const {ctx} = this;
         const areaCode = ctx.checkBody('areaCode').optional().isNumeric().len(4, 6).value;
-        const occupation = ctx.checkBody('occupation').optional().type('string').len(1, 20).value;
+        const occupation = ctx.checkBody('occupation').optional().type('string').len(0, 20).value;
         const birthday = ctx.checkBody('birthday').optional().toDate().value;
         const sex = ctx.checkBody('sex').optional().toInt().in([0, 1, 2]).value;
         const intro = ctx.checkBody('intro').optional().type('string').len(0, 200).value;
