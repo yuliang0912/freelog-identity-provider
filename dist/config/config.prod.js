f13084b40a4d3d3f597d63f07ddc965e7066fd09
yuliang <4896819@qq.com>
2022-01-24 15:11:33 +0800

    1.提交代码

--- dist/config/config.prod.js
+++ dist/config/config.prod.js
@@ -3,12 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = () => {
     const config = {};
     config.mongoose = {
-        url: 'mongodb://mongo-prod.common:27017/user'
+        url: decodeURIComponent('mongodb%3A%2F%2Fuser_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-public.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-public-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-users%3FreplicaSet%3Dmgset-58730021')
     };
-    // config.mongoose = {
-    //     url: 'mongodb://39.108.77.211:30772/node-beta'
-    // };
     config.domain = 'freelog.com';
     return config;
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLnByb2QuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29uZmlnL2NvbmZpZy5wcm9kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsa0JBQWUsR0FBRyxFQUFFO0lBQ2hCLE1BQU0sTUFBTSxHQUFRLEVBQUUsQ0FBQztJQUV2QixNQUFNLENBQUMsUUFBUSxHQUFHO1FBQ2QsR0FBRyxFQUFFLHdDQUF3QztLQUNoRCxDQUFDO0lBRUYsc0JBQXNCO0lBQ3RCLHFEQUFxRDtJQUNyRCxLQUFLO0lBRUwsTUFBTSxDQUFDLE1BQU0sR0FBRyxhQUFhLENBQUE7SUFFN0IsT0FBTyxNQUFNLENBQUM7QUFDbEIsQ0FBQyxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLnByb2QuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29uZmlnL2NvbmZpZy5wcm9kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsa0JBQWUsR0FBRyxFQUFFO0lBQ2hCLE1BQU0sTUFBTSxHQUFRLEVBQUUsQ0FBQztJQUV2QixNQUFNLENBQUMsUUFBUSxHQUFHO1FBQ2QsR0FBRyxFQUFFLGtCQUFrQixDQUFDLGdQQUFnUCxDQUFDO0tBQzVRLENBQUM7SUFFRixNQUFNLENBQUMsTUFBTSxHQUFHLGFBQWEsQ0FBQztJQUU5QixPQUFPLE1BQU0sQ0FBQztBQUNsQixDQUFDLENBQUMifQ==
--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -2,14 +2,10 @@ export default () => {
     const config: any = {};
 
     config.mongoose = {
-        url: 'mongodb://mongo-prod.common:27017/user'
+        url: decodeURIComponent('mongodb%3A%2F%2Fuser_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-public.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-public-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-users%3FreplicaSet%3Dmgset-58730021')
     };
 
-    // config.mongoose = {
-    //     url: 'mongodb://39.108.77.211:30772/node-beta'
-    // };
-
-    config.domain = 'freelog.com'
+    config.domain = 'freelog.com';
 
     return config;
 };
